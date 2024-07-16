4b85bab43768607bae5c0f405c82e5fce93a6526
Chris Krause <krause3284@gmail.com>
2024-07-16 18:00:05 +1000

    Autocommit from 2024-07-16 18:00:05
    Klipper version: v0.12.0-256-g6d700502
    
    Moonraker version: v0.8.0-379-g346a3d7
    
    Mainsail version: v2.11.2

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+architecture=""
+case $(uname -m) in
+    i386)   architecture="386" ;;
+    i686)   architecture="386" ;;
+    x86_64) architecture="amd64" ;;
+    arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+esac
+
+if [ architecture = "arm64" ]; then
+	echo "yes"
+else
+	echo "no"
+fi
